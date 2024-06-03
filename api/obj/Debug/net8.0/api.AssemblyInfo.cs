84380db106f206a33d750ee40c9e605b9a3ff265
Wojciech-Jezierski <wojtek20000@poczta.onet.pl>
2024-06-03 22:44:21 +0200

    Update Endpoint

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bef6df9e2a4f63126d36496ae7d1250c9a0feef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57000e899d268fb6e07c2e0d39dfef7ffdb42c6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
